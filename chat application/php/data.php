915726ce17f898e7307ce9a7f6a24092ce510ca4
Scotti Davide <Scotti.17115>
2022-02-24 11:21:52 +0100

    removed my contact

--- chat application/php/data.php
+++ chat application/php/data.php
@@ -1,14 +1,16 @@
 <?php
     while($row = mysqli_fetch_assoc($sql)){
-        $output .= '<a href="chat.php?user_id='. $row['unique_id'] .'">
-                        <div class="content">
-                            <img src="php/images/' . $row['img'] . '" alt="">
-                            <div class="details">
-                                <span>'. $row['fname'] . " " . $row['lname'] .'</span>
-                                <p>Click to start a chat</p>
+        if($row['unique_id'] != $_SESSION['unique_id']){
+            $output .= '<a href="chat.php?user_id='. $row['unique_id'] .'">
+                            <div class="content">
+                                <img src="php/images/' . $row['img'] . '" alt="">
+                                <div class="details">
+                                    <span>'. $row['fname'] . " " . $row['lname'] .'</span>
+                                    <p>Click to start a chat</p>
+                                </div>
                             </div>
-                        </div>
-                        <div class="status-dot"><i class="fas fa-circle"></i></div>
-                    </a>';
+                            <div class="status-dot"><i class="fas fa-circle"></i></div>
+                        </a>';
+        }
     }
 ?>
